6171244f96d6daa8a62c4bce52c031a19952f368
Juwon <reese@rmsgroup.co.kr>
2026-05-27 13:32:42 +0900

    project init

---
+++ backend/meetmap/src/main/java/com/domain/meetmap/MeetmapApplication.java
@@ -0,0 +1,13 @@
+package com.domain.meetmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetmapApplication.class, args);
+    }
+
+}
---
+++ backend/meetmap/src/test/java/com/domain/meetmap/MeetmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.meetmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
